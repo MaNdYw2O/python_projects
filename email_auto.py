51ff9ef16f730a110715a40e084b0c7f4097dfc6
Manjeet Kumar <109871402+MaNdYw2O@users.noreply.github.com>
2023-05-10 17:14:20 +0530

    Update email_auto.py
    So, we can use of this script automate our email sending things.

--- email_auto.py
+++ email_auto.py
@@ -1,14 +1,16 @@
+#import smtplibrary
 import smtplib
 
-to = input("Enter the Email address of the recipient:>>>")
+to = input("Enter the Email address of the recipient:>>>") #for Taking inputs from user 
 message = input('Enter the message you want to send to the recipient:>>>')
 
 
-def sendEmail(to, message):
-    server = smtplib.SMTP('smt.gmail.com',587)
-    server.starttls()
-    server.login('senderemail','password')
-    server.sendmail('senderemail', to , message)
-    server.close()
+def sendEmail(to, message):           #define function
+    server = smtplib.SMTP('smt.gmail.com',587) # setting up host
+    server.starttls()  # starting tls service
+    server.login('senderemail','password')   #here fill login information 
+    server.sendmail('senderemail', to , message) # here is sending function 
+    
+    server.close()   # closing session
 
-sendEmail(to, message)
+sendEmail(to, message)   # calling the function
